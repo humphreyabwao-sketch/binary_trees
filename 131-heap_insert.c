4d5b97083e181073c0546e830131bac22fbcc680
humpreyabwao-sketch <humphreyabwao@gmail.com>
2023-08-03 05:21:53 -0700

    updated file

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,90 +1,57 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Calculate the height of a binary tree.
- * @tree: Pointer to the root node of the binary tree.
+ * heap_insert - inserts a value in Max Binary Heap.
+ * @root: double pointer to the root node of the Heap to insert the value.
+ * @value: value to store in the node to be inserted.
  *
- * Return: The height of the binary tree.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-    size_t left_height, right_height;
-
-    if (tree == NULL)
-        return 0;
-
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    return (left_height > right_height ? left_height + 1 : right_height + 1);
-}
-
-/**
- * heapify_up - Restore the Max Heap property by moving the node up.
- * @node: Pointer to the node to heapify up.
- *
- * Return: Pointer to the node that was moved up.
- */
-heap_t *heapify_up(heap_t *node)
-{
-    heap_t *current = node;
-
-    while (current->parent != NULL && current->n > current->parent->n)
-    {
-        int tmp = current->n;
-        current->n = current->parent->n;
-        current->parent->n = tmp;
-
-        current = current->parent;
-    }
-
-    return current;
-}
-
-/**
- * heap_insert - Insert a value into a Max Binary Heap.
- * @root: Double pointer to the root node of the binary heap.
- * @value: The value to be inserted.
- *
- * Return: A pointer to the created node or NULL on failure.
+ * Return: pointer to the created node, or NULL on failure.
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    if (root == NULL)
-        return NULL;
+	heap_t *new_node = NULL, *tmp = NULL;
 
-    heap_t *new_node = binary_tree_node(NULL, value);
+	if (!root)
+		return (NULL);
 
-    if (new_node == NULL)
-        return NULL;
-
-    if (*root == NULL)
-    {
-        *root = new_node;
-        return new_node;
-    }
-
-    size_t tree_height = binary_tree_height(*root);
-
-    for (int level = 1; level < (int)tree_height; level++)
-    {
-        heap_t *parent = *root;
-        int pos = value;
-        while (pos > 1)
-        {
-            if (pos % 2 == 0)
-                parent = parent->left;
-            else
-                parent = parent->right;
-            pos /= 2;
-        }
-
-        if (parent->left == NULL)
-            parent->left = new_node;
-        else
-            parent->right = new_node;
-        new_node->parent = parent;
-    }
-
-    return heapify_up(new_node);
+	new_node = binary_tree_node(NULL, value);
+	if (!new_node)
+		return (NULL);
+	if (!*root)
+	{
+		*root = new_node; /* If root is empty, set the root to the new node */
+		return (new_node);
+	}
+	/* If the root is not empty, find the first available spot in the tree */
+	tmp = *root;
+	while (tmp) /* If tmp is not empty */
+	{	/* Check if the left node is empty and the right node is not */
+		if (tmp->left && !tmp->right)
+		{	/* If it is set the right node to the new node */
+			tmp->right = new_node; /*  set the parent of the new node to tmp */
+			new_node->parent = tmp;
+			break;
+		}
+		else if (!tmp->left) /* If left node is not empty and right node is */
+		{
+			tmp->left = new_node; /* set the left node to the new node */
+			new_node->parent = tmp; /* set the parent of the new node to tmp */
+			break;
+		} /* If neither of those are true */
+		/* check if left node value is less than right node value*/
+		else if (tmp->left->n < tmp->right->n)
+			tmp = tmp->left; /* If yes, set tmp to the left node */
+		else
+			tmp = tmp->right; /* If not, set tmp to the right node. */
+	}
+	/*  check if new node parent is empty */
+	while (new_node->parent && new_node->n > new_node->parent->n)
+	{	/* If yes, set new node value to parent value of the new node */
+		new_node->n = new_node->parent->n;
+		new_node->parent->n = value; /* set parent of the new node */
+		new_node = new_node->parent; /* set the new node */
+	}
+	return (new_node); /* return new node */
 }
+
+/* CODE DOESN'T PASS ALL CHECKS */
